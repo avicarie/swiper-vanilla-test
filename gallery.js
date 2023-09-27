d9bb9479fdfde8b1f1025cf3eb7be0e7a25fafd5
Sebastian Szeszko <sebastian.szeszko@primigenius.pl>
2023-09-27 20:54:23 +0200

    swiper POC

--- gallery.js
+++ gallery.js
@@ -1,15 +1,33 @@
-const galleryImages = document.querySelectorAll('.gallery img');
-const popUpDialog = document.querySelector('.pop-up-image');
-const popUpDialogCurrentImage = document.querySelector('.pop-up-image img');
-const popUpDialogCloseButton = document.querySelector('.pop-up-image span');
+const galleryImages = document.querySelectorAll(".gallery img");
+const popUpDialog = document.querySelector(".pop-up-image");
+// const popUpDialogCurrentImage = document.querySelector('.pop-up-image img');
+const popUpDialogCloseButton = document.querySelector(
+  ".pop-up-image .close-popup"
+);
+
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  keyboard: {
+    enabled: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 galleryImages.forEach((image) => {
-    image.onclick = () => {
-        popUpDialog.style.display = 'block';
-        popUpDialogCurrentImage.src = image.getAttribute('src');
-    };
+  image.onclick = () => {
+    popUpDialog.style.display = "block";
+    // popUpDialogCurrentImage.src = image.getAttribute('src');
+  };
 });
 
 popUpDialogCloseButton.onclick = () => {
-    popUpDialog.style.display = 'none';
+  popUpDialog.style.display = "none";
 };
